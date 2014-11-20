2845077595e158220f834ac729d0b27cb1862553
codechira <ruchira.86@gmail.com>
2014-11-20 07:48:20 +0530

    day-01-recommit

---
+++ VBCalendarSession/Module1.vb
@@ -0,0 +1,128 @@
+﻿Module Module1
+
+    Sub Main()
+        'What is a Program?
+        '   Data <-- --> Logic
+        '   Data 
+        '       Variables: They change their value during the program
+        '           Native Type/Value types -- stored on Stack
+        '               Fixed in size
+        '           Object type /reference type -- stored on Heap
+        '               Dynamic in size
+        '       Constants: Do not change their value
+
+        '       Lifecycle of a variable
+        '           Declare
+        '           Assign/Use 
+        '           Dispose -- not required for native type varibles
+
+        'Declare
+        Dim flag As Boolean
+        Dim count As Integer
+        Dim precision As Double
+        Dim accuracy As Single
+        Dim name As String
+
+        'Assign
+        flag = True
+        count = 10
+        precision = 10.42367575879797
+        accuracy = 10.5456
+        name = "Ruchira"
+
+        Console.WriteLine("This is a VB session Day 1")
+        Console.ReadLine()
+
+        Dim number1 As Integer
+        number1 = 20
+
+        Dim number2 As Integer
+        number2 = 30
+
+        Dim sum As Integer
+        sum = number1 + number2
+
+        Dim product As Integer
+        product = number1 * number2
+
+        Dim division As Integer
+        division = number2 / number1
+
+        Dim remainder As Integer
+        Math.DivRem(number1, number2, remainder)
+
+        'Concatenation of strings
+        Dim message1 As String
+        message1 = "This is the first day of classroom session"
+
+        Dim message2 As String
+        message2 = " and we are learning basics of VB.Net"
+
+        Dim resultmessage As String
+        resultmessage = message1 + message2
+        Console.WriteLine("The new message is " + resultmessage)
+        Console.ReadLine()
+
+        Dim position As Integer
+        position = InStr(resultmessage, "of")
+        Console.WriteLine(position)
+        Console.ReadLine()
+
+        Dim somemorefunctions As String
+        somemorefunctions = resultmessage.Substring(10)
+        'Console.WriteLine(somemorefunctions)
+        'Console.ReadLine()
+
+        'Reference Type variables
+        '   Collections
+        '       list variable
+        '       can store any data type and multiple types
+        '       dynamically grows in size
+        '       keys can be provided easily
+        '       1 based
+
+        'Declare
+        Dim pcollection As New Collection
+
+        'Use -- Adding items in a collection
+        pcollection.Add(1)
+        pcollection.Add("Ruchira")
+        pcollection.Add("Pune")
+
+        'retrieving an item from a collection
+        'Object -- consider as base class, can point to anything
+        '   late binding
+        Dim givemecollobject As Object
+        givemecollobject = pcollection.Item(2)
+        Console.WriteLine(givemecollobject)
+        Console.ReadLine()
+
+        'Declare
+        Dim pCollection2 As New Collection
+        pCollection2.Add(2)
+        pCollection2.Add("Satender")
+        pCollection2.Add("Chandigarh")
+
+        'Declare
+        Dim pRows As New Collection
+
+        'Add collections to pRow collection
+        pRows.Add(pcollection)
+        pRows.Add(pCollection2)
+
+        Dim getaRow As New Collection
+        'retrieve the second collection from pRows
+        getaRow = pRows.Item(2)
+
+        'Console.WriteLine(getaRow.Item(2))
+        Dim item As Object
+        item = getaRow.Item(2)
+
+        Console.WriteLine("The second item inside the second collection of pRows is " + item)
+        Console.ReadLine()
+
+        'Disposing
+        pcollection = Nothing
+
+    End Sub
+End Module
---
+++ VBCalendarSession/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VBCalendarSession")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Infosys Technologies Limited")> 
+<Assembly: AssemblyProduct("VBCalendarSession")> 
+<Assembly: AssemblyCopyright("Copyright © Infosys Technologies Limited 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9e0aa643-45e9-4541-b100-f47e19c2a622")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
